da561849549f2fd1a38c2dd7e2e21da56ddd5360
Seungha Lee <poopa8788@gmail.com>
2017-06-13 16:55:43 +0900

    post_create구현, post.html에 작성한 Comment목록 출력

--- django_app/post/urls.py
+++ django_app/post/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^(?P<post_pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^create/$', views.post_create, name='post_create'),
-    url(r'^(?P<post_pk>\d=)/modify/$', views.post_modify, name='post_modify')
+    url(r'^(?P<post_pk>\d=)/modify/$', views.post_modify, name='post_modify'),
+
+    # 위쪽의 결과들과 매칭되지 않을 경우
+    url(r'^.*/$', views.post_anyway, name='post_anyway'),
 ]
 
--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,6 +1,12 @@
-from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render, redirect
+from django.template import loader
 from .models import Post, Comment
+
+# 자동으로 Django에서 인증에 사용하는 User모델클래스를 리턴
+User = get_user_model()
 
 
 def post_list(request):
@@ -15,26 +21,46 @@ def post_list(request):
 
 def post_detail(request, post_pk):
     # post_pk에 해당하는 Post객체를 리턴, 보여줌
-    post = Post.objects.get(pk=post_pk)
+    try:
+        post = Post.objects.get(pk=post_pk)
+    except Post.DoesNotExist as e:
+        url = reverse('post:post_list')
+        return HttpResponseRedirect(url)
+
+    template = loader.get_template('post/post_detail.html')
     context = {
         'post': post,
     }
-    return render(request, 'post/post_detail.html', context)
+    rendered_string = template.render(context=context, request=request)
+
+    return HttpResponse(rendered_string)
 
 
 def post_create(request):
     # POST요청을 받아 Post객체를 생성 후 Post_list페이지로 redirect
-    if request.method == 'GET':
+    if request.method == 'POST':
+        # get_user_model을 이용해서 얻은 User클래스(Django에서 인증에 사용하는  유저모델)에서 임의의 유저 한명을 가져온다.
+        user = User.objects.first()
+        # 새 Post객체를 생성하고 DB에 저장
+        post = Post.objects.create(
+            author=user,
+            # file은 POST요청시 input[type='file']이 가진 name속성
+            photo=request.FILES['file'],
+        )
+        # POST요청시 name이 comment인 input에서 전달된 값을 가져옴
+        comment_string = request.POST.get('comment', '')
+        if not comment_string:
+            # 댓글로 사용할 문자열이 전달된 경우 위에서 생성한 post 객체에 연결되는 Comment객체를 생성해준다
+            post.comment_set.create(
+                # 임의의 user를 사용하므로 나중에 실제 로그인된 사용자로 바꿔야함
+                author=user,
+                content=comment_string,
+            )
+
+        return redirect('post:post_detail', post_pk=Post.pk)
+
+    else:
         return render(request, 'post/post_create.html')
-
-    elif request.method == 'POST':
-        data = request.POST
-        photo = request.FILES['photo']
-        post = Post.objects.create(author=user)
-        return redirect('post_list')
-
-    pass
-
 
 
 def post_modify(request, post_pk):
@@ -53,10 +79,7 @@ def post_delete(request, post_pk):
         return HttpResponse('method "GET"은 허용되지않습니다.')
 
 
-
 def comment_create(request, post_pk):
-    if request.method == 'POST':
-        return redirect(post_detail)
     # POST요청을 받아 Comment객체를 생성 후 post_detail페이지로 redirect
     pass
 
@@ -66,7 +89,7 @@ def comment_modify(request, post_pk):
 
 
 def comment_delete(request, post_pk):
-    if request.method == 'POST':
-        comment = Comment.objects.get(pk=post_pk)
-        comment.delete()
-        return redirect(post_list)
+    pass
+
+def post_anyway(request):
+    return redirect('post:post_list')
