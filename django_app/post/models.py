37334cb4eea4bf4c140cef5d8d4648ff096b214f
Seungha Lee <poopa8788@gmail.com>
2017-06-20 16:30:37 +0900

    태그생성 정규표현식 수정, 태그 css설정

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -72,6 +72,8 @@ class Comment(models.Model):
     )
 
     def save(self, *args, **kwargs):
+        if not self.pk:
+            super().save(*args, **kwargs)
         self.make_html_content_and_add_tags()
         super().save(*args, **kwargs)
 
@@ -79,34 +81,33 @@ class Comment(models.Model):
         # 박보영 <a href='#'>#여신</a> <a href='#'>#존예</a> 인스타
         # 해당내용을 self.html_content에 대입
 
+    def make_html_content_and_add_tags(self):
+        # 해시태그에 해당하는 정규표현식
+        p = re.compile(r'(#\w+)')
+        # findall매서드로 해시태그 문자열들을 가져옴
+        tag_name_list = re.findall(p, self.content)
+        # 기존 content(Content내용)을 변수에 할당
+        ori_content = self.content
+        for tag_name in tag_name_list:
+            # Tag객체를 가져오거나 생성, 생성여부는 쓰지않는 변수이므로 _ 처리
+            tag, _ = Tag.objects.get_or_create(name=tag_name.replace('#', ''))
+            change_tag = '<a href="#" class="hash-tag">{}</a>'.format(
+                tag_name
+            )
 
-        def make_html_content_and_add_tags(self):
-            # 해시태그에 해당하는 정규표현식
-            p = re.compile(r'(#\w+)')
-            # findall매서드로 해시태그 문자열들을 가져옴
-            tag_name_list = re.findall(p, self.content)
-            # 기존 content(Content내용)을 변수에 할당
-            ori_content = self.content
-            for tag_name in tag_name_list:
-                # Tag객체를 가져오거나 생성, 생성여부는 쓰지않는 변수이므로 _ 처리
-                tag, _ = Tag.objects.get_or_create(name=tag_name.replace('#', ''))
+            ori_content = re.sub(r'{}(?![<\w])'.format(tag_name), change_tag, ori_content, count=1)
+            # content에 포함된 Tag목록을 자신의 tags필드에 추가
+            if not self.tags.filter(pk=tag.pk).exists():
+                self.tags.add(tag)
+        # 편집이 완료된 문자열을 html_content에 저장
+        self.html_content = ori_content
 
-                ori_content = ori_content.replace(
-                    tag_name,
-                    '<a href="#" class="hash-tag">{}</a>'.format(
-                        tag_name
-                    )
-                )
-                # content에 포함된 Tag목록을 자신의 tags필드에 추가
-                if not self.tags.filter(pk=tag.pk).exist():
-                    self.tags.add(tag)
-            # 편집이 완료된 문자열을 html_content에 저장
-            self.html_content = ori_content
 
 class CommentLike(models.Model):
     comment = models.ForeignKey(Comment)
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     created_date = models.DateTimeField(auto_now_add=True)
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=50)
