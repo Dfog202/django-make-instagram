ee210d1f53813815b4db97573c17dd321c9cd91d
Seungha Lee <poopa8788@gmail.com>
2017-06-20 12:57:28 +0900

    post_modify 뷰 생성 및 url연결, 관련 css 추가

--- django_app/post/views/comment.py
+++ django_app/post/views/comment.py
@@ -1,10 +1,10 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
 
 from post.forms import CommentForm
-from post.models import Post
+from post.models import Post, Comment
 
 __all__ = (
     'comment_create',
@@ -37,9 +37,17 @@ def comment_create(request, post_pk):
 
 
 @login_required
-def comment_modify(request, post_pk):
+def comment_modify(request, comment_pk):
     # CommentForm을 만들어서 해당 ModelForm안에서 생성/수정 가능하도록
-    pass
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    if request.method == 'POST':
+        pass
+    else:
+        form = CommentForm(instance=comment)
+    context = {
+        'form': form,
+    }
+    return render(request, 'post/comment_modify.html', context)
 
 
 @login_required
