a4cc330c19ae5ffeedc9fa5b3a001893a3e5466a
Seungha Lee <poopa8788@gmail.com>
2017-06-22 11:18:01 +0900

    custom filter구현 및 comment 등록시 page등 query포함 이전페이지로 이동, fallow관계 설명 추가

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -84,6 +84,7 @@ INSTALLED_APPS = [
 
     'post',
     'member',
+    'utils',
 ]
 
 MIDDLEWARE = [
--- django_app/member/models.py
+++ django_app/member/models.py
@@ -3,6 +3,27 @@ from django.db import models
 
 
 class User(AbstractUser):
+    '''
+    동작
+        fallow : 내가 다른사람을 fallow
+        unfallow : 내가 다른사람에게 한 fallow를 취소함
+
+    속성
+        fallowers : 나를 fallow하고 있는 사람들
+        fallower : 나를 fallow하고 있는 사람
+        fallowing : 내가 fallow하고 있는 사람들
+        없음 : 내가 fallow하고 있는 사람 1명
+
+
+    ex) 내가 박보영, 최유정을 fallow하고 고성현과 심수정은 나를 fallow한다
+        나의 fallowers는 고성현, 김수정
+        나의 fallowing은 박보영, 최유정
+        김수정은 나의 fallower이다
+        나는 박보영의 fallower다
+        나와 고성현은 friend단계이다
+        나의 friends는 고성현 1명이다.
+
+    '''
     # 이 User모델을 AUTH_USER_MODEL로 사용하여
     nickname = models.CharField(max_length=24, null=True, unique=True)
 
