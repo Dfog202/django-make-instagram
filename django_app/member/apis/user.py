b8a544047d821ccfecfecfd1687d08c19ecaa919
Seungha Lee <poopa8788@gmail.com>
2017-07-11 15:50:09 +0900

    UserSerializer, UserCreationSerializer 구현

--- django_app/member/apis/user.py
+++ django_app/member/apis/user.py
@@ -1,24 +1,63 @@
-from rest_framework import status
+from rest_framework import status, permissions, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from utils.permissions import ObjectIsRequestUser
 from ..models import User
-from ..serializers import UserSerializer
+from ..serializers import UserSerializer, UserCreationSerializer
 
 __all__ = (
-    'UserDetailView',
+    'UserRetrieveUpdateDestroyView',
+    'UserListCreateView',
 )
 
 
-class UserDetailView(APIView):
-    @staticmethod
-    def get_object(pk):
-        try:
-            return User.objects.get(pk=pk)
-        except User.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
 
-    def get(self, request, pk):
-        user = self.get_object(pk)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return UserSerializer
+        elif self.request.method == 'POST':
+            return UserCreationSerializer
 
+
+# UserListCreatView
+# generice.ListCreateAPIView 사용
+class UserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        ObjectIsRequestUser,
+    )
+
+
+
+    # @staticmethod
+    # def get_object(pk):
+    #     try:
+    #         return User.objects.get(pk=pk)
+    #     except User.DoesNotExist:
+    #         return Response(status=status.HTTP_404_NOT_FOUND)
+    #
+    # def get(self, request, pk):
+    #     user = self.get_object(pk)
+    #     serializer = UserSerializer(user)
+    #     return Response(serializer.data)
+    #
+    # def put(self, request, pk):
+    #     user = self.get_object(pk)
+    #     serializer = UserSerializer(user, request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #
+    # def patch(self, request, pk):
+    #     user = self.get_object(pk)
+    #     serializer = UserSerializer(user, request.data, partial=True)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #
