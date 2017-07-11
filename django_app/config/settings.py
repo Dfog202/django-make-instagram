cd93865debc256e0b573fb8a920bb89c31fa187b
Seungha Lee <poopa8788@gmail.com>
2017-07-11 11:43:46 +0900

    DB sqlite3 으로 변경 ㅠ

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -131,13 +131,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'instagram',
-        'USER': 'lee',
-        'PASSWORD': 'asd123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'test': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
